ea900ad097e964eb0d4baf5006cb71490dfe8b61
sksamuel <sam@sksamuel.com>
2021-03-09 06:56:03 -0600

    Project migrated from kotest main repo

---
+++ build.gradle.kts
@@ -0,0 +1,152 @@
+buildscript {
+   repositories {
+      jcenter()
+      mavenCentral()
+      maven {
+         url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+      }
+      maven {
+         url = uri("https://plugins.gradle.org/m2/")
+      }
+   }
+}
+
+plugins {
+   java
+   `java-library`
+   id("java-library")
+   id("maven-publish")
+   signing
+   maven
+   `maven-publish`
+   id("org.jetbrains.dokka") version Libs.dokkaVersion
+   kotlin("multiplatform").version(Libs.kotlinVersion)
+}
+
+repositories {
+   mavenLocal()
+   mavenCentral()
+   maven {
+      url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+   }
+   maven(url = "https://kotlin.bintray.com/kotlinx/") // soon will be just jcenter()
+   jcenter()
+}
+
+group = Libs.org
+version = Ci.version
+
+kotlin {
+
+   targets {
+
+      jvm {
+         compilations.all {
+            kotlinOptions {
+               jvmTarget = "1.8"
+            }
+         }
+      }
+
+      js {
+         browser()
+         nodejs()
+      }
+
+      linuxX64()
+
+      mingwX64()
+
+      macosX64()
+      tvos()
+//      watchos()
+
+      iosX64()
+      iosArm64()
+      iosArm32()
+   }
+
+   sourceSets {
+
+      val commonMain by getting {
+         dependencies {
+            implementation(Libs.Kotest.assertionsShared)
+            implementation(Libs.Ktor.clientCore)
+         }
+      }
+
+      val jvmMain by getting {
+         dependsOn(commonMain)
+         dependencies {
+            implementation(Libs.Ktor.serverCore)
+            implementation(Libs.Ktor.serverTestHost)
+         }
+      }
+
+      val jvmTest by getting {
+         dependsOn(jvmMain)
+         dependencies {
+            implementation(Libs.Kotest.junit5)
+         }
+      }
+
+      val desktopMain by creating {
+         dependsOn(commonMain)
+      }
+
+      val macosX64Main by getting {
+         dependsOn(desktopMain)
+      }
+
+      val mingwX64Main by getting {
+         dependsOn(desktopMain)
+      }
+
+      val linuxX64Main by getting {
+         dependsOn(desktopMain)
+      }
+
+      val iosX64Main by getting {
+         dependsOn(desktopMain)
+      }
+
+      val iosArm64Main by getting {
+         dependsOn(desktopMain)
+      }
+
+      val iosArm32Main by getting {
+         dependsOn(desktopMain)
+      }
+
+//      val watchosMain by getting {
+//         dependsOn(desktopMain)
+//      }
+
+      val tvosMain by getting {
+         dependsOn(desktopMain)
+      }
+
+      all {
+         languageSettings.useExperimentalAnnotation("kotlin.time.ExperimentalTime")
+         languageSettings.useExperimentalAnnotation("kotlin.experimental.ExperimentalTypeInference")
+      }
+   }
+}
+
+tasks.named<Test>("jvmTest") {
+   useJUnitPlatform()
+   filter {
+      isFailOnNoMatchingTests = false
+   }
+   testLogging {
+      showExceptions = true
+      showStandardStreams = true
+      events = setOf(
+         org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+         org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
+      )
+      exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+   }
+}
+
+apply("./publish-mpp.gradle.kts")
---
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -0,0 +1,24 @@
+object Libs {
+
+   const val kotlinVersion = "1.4.31"
+   const val org = "io.kotest.extensions"
+   const val dokkaVersion = "0.10.1"
+
+   object Kotest {
+      private const val version = "4.4.3"
+      const val assertionsShared = "io.kotest:kotest-assertions-shared:$version"
+      const val api = "io.kotest:kotest-framework-api:$version"
+      const val junit5 = "io.kotest:kotest-runner-junit5-jvm:$version"
+   }
+
+   object Ktor {
+      private const val version = "1.5.1"
+      const val serverCore = "io.ktor:ktor-server-core:$version"
+      const val serverTestHost = "io.ktor:ktor-server-test-host:$version"
+      const val clientJs = "io.ktor:ktor-client-js:$version"
+      const val clientCore = "io.ktor:ktor-client-core:$version"
+      const val clientCoreJvm = "io.ktor:ktor-client-core-jvm:$version"
+      const val clientCioJvm = "io.ktor:ktor-client-cio-jvm:$version"
+      const val clientApache = "io.ktor:ktor-client-apache:$version"
+   }
+}
