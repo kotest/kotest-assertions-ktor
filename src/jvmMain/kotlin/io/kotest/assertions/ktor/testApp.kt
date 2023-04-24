a3d9e67a566235df668ec651256f06a301a612e0
sksamuel <sam@sksamuel.com>
2023-04-24 07:23:51 -0500

    Added configure

---
+++ src/jvmMain/kotlin/io/kotest/assertions/ktor/testApp.kt
@@ -0,0 +1,41 @@
+package io.kotest.assertions.ktor
+
+import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.engine.HttpClientEngineConfig
+import io.ktor.server.application.Application
+
+class Configure {
+
+   internal var configureClient: HttpClientConfig<out HttpClientEngineConfig>.() -> Unit = {}
+   internal var configureServer: Application.() -> Unit = {}
+
+   fun client(block: (HttpClientConfig<out HttpClientEngineConfig>.() -> Unit)) {
+      configureClient = block
+   }
+
+   fun server(block: Application.() -> Unit) {
+      configureServer = block
+   }
+}
+
+fun configure(block: Configure.() -> Unit): Configure {
+   val config = Configure()
+   config.block()
+   return config
+}
+
+class ApplicationTest(val client: HttpClient)
+
+suspend fun Configure.testApplication(test: suspend ApplicationTest.() -> Unit) {
+   val configure = this
+   io.ktor.server.testing.testApplication {
+      application {
+         configure.configureServer(this)
+      }
+      val client = createClient {
+         configure.configureClient(this)
+      }
+      ApplicationTest(client).test()
+   }
+}
