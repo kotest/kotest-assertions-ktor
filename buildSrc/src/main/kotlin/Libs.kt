05f0598f8c5d2e173094a26d4924dce182e1892c
sksamuel <sam@sksamuel.com>
2023-04-24 06:39:12 -0500

    Updated ktor version to 2.3.0

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -12,7 +12,7 @@ object Libs {
    }
 
    object Ktor {
-      private const val version = "2.2.3"
+      private const val version = "2.3.0"
       const val serverCore = "io.ktor:ktor-server-core:$version"
       const val serverTestHost = "io.ktor:ktor-server-test-host:$version"
       const val clientCore = "io.ktor:ktor-client-core:$version"
